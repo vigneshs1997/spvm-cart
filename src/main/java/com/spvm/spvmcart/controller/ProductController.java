5c2cdca748614755e5ddac6215fffd12fd8ca7a9
vigneshs1997 <vigneshmaster1997@gmail.com>
2026-03-20 23:31:17 +0530

    Pagination is given to the application

--- src/main/java/com/spvm/spvmcart/controller/ProductController.java
+++ src/main/java/com/spvm/spvmcart/controller/ProductController.java
@@ -3,12 +3,16 @@ package com.spvm.spvmcart.controller;
 import com.spvm.spvmcart.entity.Product;
 import com.spvm.spvmcart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController//This Controller for REST APIs
 @RequestMapping("/api/products")//Parent endpoint
@@ -17,8 +21,9 @@ public class ProductController {
     @Autowired
     ProductService productService;
     @GetMapping()
-    public ResponseEntity<List<Product>> getAllProducts(){
-           List<Product> products =  productService.getAllProducts();
-           return ResponseEntity.ok(products);//Return in the form of JSON
+    public Map<String, Object> getAllProducts(@RequestParam(defaultValue = "0") int pageNumber,//page starts from 0
+                                                              @RequestParam(defaultValue = "5") int pagesize){//5 objects in the page
+
+           return productService.getAllProducts(pageNumber,pagesize);//Return in the form of JSON
     }
 }
--- src/main/java/com/spvm/spvmcart/service/ProductService.java
+++ src/main/java/com/spvm/spvmcart/service/ProductService.java
@@ -3,17 +3,25 @@ package com.spvm.spvmcart.service;
 import com.spvm.spvmcart.entity.Product;
 import com.spvm.spvmcart.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepo productRepo;//Instance of ProductRepo (Bean Object)
 
-    public List<Product> getAllProducts(){
-        List<Product> products = productRepo.findAll();
-        return products;
+    public Map<String, Object> getAllProducts(int pageNumber, int pagesize){//pageNumber starts 0, content in page is EX:5
+        Pageable pageable = PageRequest.of(pageNumber,pagesize);//PageRequest class return object of Pageable interface
+        Page<Product> products = productRepo.findAll(pageable);//it will return the page of products
+        Map<String,Object> response = new HashMap();
+        response.put("products", products.getContent());//getContent() return Object (group of data from a page) in the JSON Object
+        response.put("totalProducts",products.getTotalElements());//getTotalElements() return total product count EX:10
+        return response;
     }
 }
