75d7e7fa31cd90423d7e540010e4147443bbb584
vigneshs1997 <vigneshmaster1997@gmail.com>
2026-03-21 12:50:10 +0530

    Fetching Prouduct by ID and Category is added

--- src/main/java/com/spvm/spvmcart/controller/ProductController.java
+++ src/main/java/com/spvm/spvmcart/controller/ProductController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -17,13 +14,20 @@ import java.util.Map;
 @RestController//This Controller for REST APIs
 @RequestMapping("/api/products")//Parent endpoint
 public class ProductController {
+
     //Otherwise bean object for ProductService will not be created
     @Autowired
     ProductService productService;
-    @GetMapping()
+
+    @GetMapping
     public Map<String, Object> getAllProducts(@RequestParam(defaultValue = "0") int pageNumber,//page starts from 0
                                                               @RequestParam(defaultValue = "5") int pagesize){//5 objects in the page
 
            return productService.getAllProducts(pageNumber,pagesize);//Return in the form of JSON
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id){
+       return productService.getProductById(id);
     }
 }
